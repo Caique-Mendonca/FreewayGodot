fc78e60a63d5f7fd9f7f1a99ffa399e14219a25d
Caique-Mendonca <caique4874@gmail.com>
2025-08-29 19:47:48 -0300

    a

--- Cenas/main.gd
+++ Cenas/main.gd
@@ -5,7 +5,6 @@ var pistas_rapidas_y = [104, 272, 488]
 var pistas_lentas_y = [160, 216, 324, 384, 438, 544, 600]
 var score = 0
 var score2 = 0
-var timer = 60
 
 func _ready() -> void:
 	$HUD/Placar.text = str(score)
@@ -19,8 +18,8 @@ func _on_timer_carros_rapidos_timeout() -> void:
 	var carro = cena_carros.instantiate()
 	add_child(carro)
 	var pista_y = pistas_rapidas_y[randi_range(0, pistas_rapidas_y.size() - 1)]
-	carro.position = Vector2(-10, pista_y)
-	carro.set_linear_velocity(Vector2(randf_range(700.0, 720.0),0))
+	carro.position = Vector2(1152, pista_y)
+	carro.set_linear_velocity(Vector2(randf_range(-700.0, -720.0),0))
 	carro.set_linear_damp(0.0)
 
 func _on_timer_carros_lentos_timeout() -> void:
